e635321822edc17d8b763e7416e6c348a9b3a9f4
Anupal Das <dasanupal24@gmail.com>
2024-01-16 22:13:31 +0530

    added message is simplearraysum.c

--- solvemefirst.c
+++ solvemefirst.c
@@ -1,22 +1,25 @@
+// Defines a function solveMeFirst that adds two integers.
+// Reads two numbers (num1 and num2) from the user.
+// Calls solveMeFirst with those numbers, receiving the sum.
+// Stores the sum in variable sum.
+// Prints the value of sum (the combined numbers).
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 
 int solveMeFirst(int a, int b) {
-    // returns a + b    
     return a + b;
 }
 
 int main() {
-    // Read input
+
     int num1, num2;
     scanf("%d %d", &num1, &num2);
 
-    // Calc Sum
     int sum = solveMeFirst(num1, num2);
 
-    // Print Sum
     printf("%d", sum);
 
     return 0;
